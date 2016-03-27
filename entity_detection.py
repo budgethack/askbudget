4ce6855fb2d9215eac93716b8c59ab83afab9b13
Lex Toumbourou <lextoumbourou@gmail.com>
2016-03-27 20:22:45 +1100

    Add entity detection stuff.

---
+++ entity_detection.py
@@ -0,0 +1,22 @@
+from watson_developer_cloud import AlchemyLanguageV1
+
+from elasticsearch.helpers import scan
+from elasticsearch import Elasticsearch
+
+import private
+
+if __name__ == '__main__':
+    es = Elasticsearch()
+    al = AlchemyLanguageV1(api_key=private.ENTITY_API_KEY)
+    combined_operations = ['entity', 'keyword', 'taxonomy', 'concept']
+
+    for hit in scan(es, index='budgethack'):
+        source = hit['_source']
+
+        if hit['_source']['text']:
+            result = al.combined(
+                text=hit['_source']['text'], extract=combined_operations, sentiment=True)
+            source.update(result)
+            es.index(
+                id=hit['_id'], index='budgethack',
+                doc_type=hit['_type'], body=source)
