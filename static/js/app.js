950d20331299746333510dce4b2c6a73a084b377
Lex Toumbourou <lextoumbourou@gmail.com>
2016-04-03 16:24:53 +1000

    Add routing.

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,58 @@
-var app = angular.module('search', ['angular-jqcloud']);
+var app = angular.module('search', ['ngRoute', 'angular-jqcloud']);
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/home', {
+        templateUrl: '/static/partials/home.html',
+        controller: 'HomeCtrl'
+      }).
+      when('/answer', {
+        templateUrl: '/static/partials/answer.html',
+        controller: 'AnswerCtrl'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }
+]);
 
-app.controller('queryListCtrl', ['$scope','$filter', '$http', '$location', function($scope, $filter, $http, $location) {
-        $scope.answer;
-        $scope.words = [];
+app.controller('QueryCtrl', function($scope, $rootScope) {
+  console.log('Here I am');
+});
 
-        $scope.sendQuestion = function() {
-          console.log($scope.question);
-            $http.post('api/post_question', {
-              'question': $scope.question
-            }).then(function successCallback(response) {
-                $scope.answer = response.data.answer;
-            });
-        };
+app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
+  console.log('AT HOME');
 
-        $http.get('/api/get_concepts').then(function successCallback(response) {
-            words = [];
-            angular.forEach(response.data.main_concepts, function(value, key) {
-              words.push({"text": value.text, "weight": value.weight, "handlers": {
-                click: function(clicked) {
-                   $scope.question = clicked.currentTarget.innerText;
-                   $scope.sendQuestion();
-               }}, "link": "#!?question=" + value.text})
-            });
+  $http.get('/api/get_concepts').then(function successCallback(response) {
+    words = [];
 
-            $scope.words = words;
-        });
+    angular.forEach(response.data.main_concepts, function(value, key) {
+      words.push({
+        "text": value.text, "weight": value.weight,
+        "link": "#/answer?question=" + value.text})
+    });
+
+    $scope.words = words;
+  });
+
 }]);
+
+app.controller(
+    'AnswerCtrl', function($scope, $filter, $http, $routeParams, $rootScope) {
+  $scope.answer;
+  $scope.words = [];
+
+  sendQuestion = function(question) {
+      $http.post('api/post_question', {
+        'question': question
+      }).then(function successCallback(response) {
+          $scope.answer = response.data.answer;
+      });
+  }
+
+  if ($routeParams.question) {
+    $rootScope.question = $routeParams.question; 
+    sendQuestion($routeParams.question);
+  };
+
+});
