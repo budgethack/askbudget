fb17f90a4ef4ed9af122612935b272a61e0fbb94
Lex Toumbourou <lextoumbourou@gmail.com>
2016-04-03 17:02:31 +1000

    Fixed chart.

--- static/js/app.js
+++ static/js/app.js
@@ -59,88 +59,27 @@ app.controller(
   chart1.type = "ColumnChart";
   chart1.cssStyle = "height:300px; width:100%";
   chart1.data = {"cols": [
-      {id: "category", label: "Year", type: "string"},
-      {id: "families", label: "Families", type: "number"},
-      {id: "jobs", label: "Jobs", type: "number"},
-      {id: "health", label: "Health", type: "number"},
-      {id: "education", label: "Education", type: "number"},
-      {id: "trade", label: "Trade", type: "number"},
-      {id: "small-business", label: "Small business", type: "number"},
-      {id: "transportation", label: "Transportation", type: "number"},
+    {id: 'year', label: 'Year', type: 'string'},
+    {id: 'mentions', label: 'Mentions', type: 'number'}
   ], "rows": [
       {c: [
           {v: "2011"},
-          {v: 10, f: "Majority are in Budget Overview  Doc,Suburban Growth Doc"},
-          {v: 11},
-          {v: 7},
-          {v: 4},
-          {v: 4},
-          {v: 2},
-          {v: 6}
+          {v: 100},
       ]},
       {c: [
           {v: "2012"},
-          {v: 13},
-          {v: 3},
-          {v: 10},
-          {v: 2},
-          {v: 4},
-          {v: 3},
-          {v: 5}
+          {v: 130},
       ]},
       {c: [
-          {v: "2013"},
-          {v: 9},
-          {v: 0},
-          {v: 11},
-          {v: 6},
-          {v: 6},
-          {v: 3},
-          {v: 3}
-      ]},
-      {c: [
-          {v: "2014"},
-          {v: 8},
-          {v: 3},
-          {v: 11},
-          {v: 6},
-          {v: 4},
-          {v: 2},
-          {v: 6}
-
-      ]},
-      {c: [
-          {v: "2015"},
-          {v: 9},
-          {v: 10},
-          {v: 12},
-          {v: 9},
-          {v: 3},
-          {v: 4},
-          {v: 5}
-
-      ]},
-      {c: [
-          {v: "2016"},
-          {v: 9},
-          {v: 3},
-          {v: 8},
-          {v: 10},
-          {v: 2},
-          {v: 4},
-          {v: 2}
-
+          {v: "2011"},
+          {v: 90},
       ]}
   ]};
 
   chart1.options = {
-      "title": "Victoria Budget Data per yearly",
       "isStacked": "true",
       "fill": 20,
       "displayExactValues": true,
-      "vAxis": {
-          "title": "Budget Information in Percentage", "gridlines": {"count": 6}
-      },
       "hAxis": {
           "title": "Year"
       }
