8958590c4aa4d392a0fc48be66112dc241e1a2f5
Lex Toumbourou <lextoumbourou@gmail.com>
2016-04-05 09:36:20 +1000

    Add cache to Entity Detection script.

--- scripts/entity_detection.py
+++ scripts/entity_detection.py
@@ -9,8 +9,12 @@ from elasticsearch import Elasticsearch
 
 import private
 
+import shelve
+
 if __name__ == '__main__':
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+    cache = shelve.open('./entity-cache')
 
     es = Elasticsearch([private.ELASTICSEARCH_HOST])
     al = AlchemyLanguageV1(api_key=private.ENTITY_API_KEY)
@@ -18,14 +22,22 @@ if __name__ == '__main__':
 
     for hit in scan(es, index='budgethack'):
         source = hit['_source']
+        text = hit['_source']['text']
         
         print 'working...'
-        if hit['_source']['text']:
-            result = al.combined(
-                text=hit['_source']['text'], extract=combined_operations,
-                sentiment=True)
-            source.update(result)
+        if text:
+            if not cache.has_key(text.encode('utf8')):
+                try:
+                    result = al.combined(
+                        text=hit['_source']['text'], extract=combined_operations,
+                        sentiment=True)
+                except Exception as e:
+                    print "Error occured. Skipping. ({0})".format(e)
+                    continue
+                cache[text.encode('utf8')] = result
+
+            source.update(cache[text.encode('utf8')])
+            print "ABOUT TO INGEST", source
             es.index(
                 id=hit['_id'], index='budgethack',
                 doc_type=hit['_type'], body=source)
-        
